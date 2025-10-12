4e30710e93ac59d2c627fe2104c4e100e9aa0822
ming <matwming114@gmail.com>
2025-10-12 13:39:20 +1100

    chore: add and setup clientside state manager jotai

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,12 @@
 'use client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {Provider as JotaiProvider} from 'jotai';
 
-export const Providers=({children}:{children:React.ReactNode})=>{
+export const Providers = ({children}: { children: React.ReactNode }) => {
     const queryClient = new QueryClient();
     return <QueryClientProvider client={queryClient}>
-        {children}
+        <JotaiProvider>
+            {children}
+        </JotaiProvider>
     </QueryClientProvider>;
 }
