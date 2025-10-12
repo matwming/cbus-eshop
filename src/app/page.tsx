7d12a09c09e2f7243c2b10c8db3d0fc8287ae91f
ming <matwming114@gmail.com>
2025-10-12 13:15:10 +1100

    chore: setup tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>
+  return <div className="text-3xl font-bold underline">
       hello world cbus eshop
   </div>;
 }
